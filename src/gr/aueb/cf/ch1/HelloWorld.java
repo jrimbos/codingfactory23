28a0257c8586ae54852d05ba2cd6cc2738d8dca2
RimbosG <jrimbos15@gmail.com>
2023-06-06 19:48:27 +0300

    v1.2

--- src/gr/aueb/cf/ch1/HelloWorld.java
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -1,13 +1,25 @@
 package gr.aueb.cf.ch1;
 
+import gr.aueb.cf.ch1.model.Point;
+// import java.util.Scanner;
+// import static java.lang.Math.abs;
+
 /**
- * Εκτυπώνει Hello World! στο
- * standard output (κονσόλα) .
- *
+ * Prints <b>Hello World!</b> in
+ * standard output.
+ *Defines a {@link HelloWorld} type.
  * @author a8ana
  */
 public class HelloWorld {
+
+    private java.util.Scanner sc = new java.util.Scanner(System.in);
+    private Point point;
+    private String s;
+
+
     public static void main (String[] args) {
+
+        int abs = Math.abs(-3);
         System.out.println("Hello World!");
     }
 }
---
+++ src/gr/aueb/cf/ch11/ImmutablePoint3D.java
@@ -0,0 +1,4 @@
+package gr.aueb.cf.ch11;
+
+public class ImmutablePoint3D {
+}
